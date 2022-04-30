1baf5b9c9454d60242f0340dfd3531ae9663d12d
Luis Ortiz <luiann.ortiz@gmail.com>
2022-04-30 11:41:11 -0500

    Add StudenService requirements

---
+++ test/services/StudentService.test.js
@@ -0,0 +1,30 @@
+const Student = require("../../lib/models/Student");
+const StudentService = require("../../lib/services/StudentService");
+
+describe(` Test to Student Service`, () => {
+
+    test(`1. get all Students`, () => {
+        const students = [ {name: "Angel", certificate: true}, {name: "Maria", certificate: false }];
+        const objStudent = StudentService.allStudents(students);
+        expect(objStudent).toBeInstanceOf(Student);
+    });
+
+    test(`2. get the email of all students have certification`, () => {
+        const students = [ {name: "Angel", email: "angel@gmail.com", haveCertification: true}, 
+                            {name: "Maria", email: "maria@gmail.com", haveCertification: false },
+                            {name: "Juan", email: "juan@gmail.com", haveCertification: true }];
+        const state = true;
+        const studentEmail = StudentService.certifiedStudentsMail(students, state);
+        expect(studentEmail[0]).toBe("angel@gmail.com");
+        
+    });
+
+    test(`3. get all students that have more than 500 credits`, () => {
+        const students = [ {name: "Angel", credits: 46}, {name: "Maria", credits: 590 },{name: "Juan", credits: 30 }, {name: "Juan", credits: 890 }];
+        const credits = 500;
+        const studentsCredit = StudentService.filterStudentByCredits(students, credits)
+        
+        expect(studentsCredit[0].credits).toBe(590)
+    });
+
+});
--- test/utils/Reader.test.js
+++ test/utils/Reader.test.js
@@ -1,11 +1,18 @@
 const Reader = require("./../../lib/utils/Reader");
+const Student = require("./../../lib/models/Student");
 
 describe(`Testint reader file`, () => {
 
     test(` 1. read json file`, () => {
         const students = "visualpartners.json"
         const fileStudents = Reader.readJsonFile(students);
-    
         expect(fileStudents).not.toBeNull();
+    });
+
+    test(` 2. match with Student Class`, () => {
+        const students = "visualpartners.json"
+        const fileStudents = Reader.readJsonFile(students);
+        const student = new Student(fileStudents[0])
+        expect(student).toBeInstanceOf(Student);
     })
 })
