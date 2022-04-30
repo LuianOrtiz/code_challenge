e3aa4975a0e3a3917b3f113cb09644b87dc450cf
Luis Ortiz <luiann.ortiz@gmail.com>
2022-04-30 13:11:01 -0500

    Add Student Controller finished

---
+++ test/controllers/StudentController.test.js
@@ -0,0 +1,27 @@
+const StudentController = require("../../lib/controllers/StudentController");
+
+describe(`Test to Student Controller`, () => {
+
+    test(`1. get All Students`, () => {
+        const students = StudentController.getAllStudents();
+        
+        expect(students).not.toBeUndefined();
+    });
+
+    test(`2. get all student's email than have certification`, () => {
+        const haveCertification = true
+        const studentEmail = StudentController.getEmailStudents(haveCertification);
+        const regex = [
+            expect.stringMatching(/^\w+([\.-]?\w+)@visualpartnership.xyz$/)
+        ]
+       expect(studentEmail).toEqual(
+            expect.arrayContaining(regex)
+        );
+    });
+
+    test(`3. get credit's students > 500`, () => {
+        const credits = 500;
+        const creditStudents = StudentController.getStudentCredits(credits);
+        expect(creditStudents[0].credits).toBe(508);
+    })
+});
